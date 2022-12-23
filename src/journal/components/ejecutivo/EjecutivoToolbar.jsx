9059810b7f09e3a8a044a11d3c5c560eb7802051
NelsonKyros <nelson.soto@kyros.cl>
2022-12-23 12:01:49 -0300

    Pauta Toolbar

--- src/journal/components/ejecutivo/EjecutivoToolbar.jsx
+++ src/journal/components/ejecutivo/EjecutivoToolbar.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+// import React from 'react'
+
+// export const EjecutivoToolbar = () => {
+//   return (
+//     <div>ejecutivo-toolbar</div>
+//   )
+// }
+
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
 
 export const EjecutivoToolbar = () => {
   return (
-    <div>ejecutivo-toolbar</div>
-  )
+    <Stack direction="row" spacing={2}>
+      <Button variant="contained">Contained</Button>
+      <Button variant="contained" disabled>
+        Disabled
+      </Button>
+      <Button variant="contained" href="#contained-buttons">
+        Link
+      </Button>
+    </Stack>
+  );
 }
