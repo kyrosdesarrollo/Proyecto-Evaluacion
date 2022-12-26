25eae88130800c35f89fe463b968d0477d191df0
NelsonKyros <nelson.soto@kyros.cl>
2022-12-26 15:08:33 -0300

    Actualizacion Registro

---
+++ src/store/menu/menuSlice.js
@@ -0,0 +1,58 @@
+import { createSlice } from '@reduxjs/toolkit';
+export const journalSlice = createSlice({
+    name: 'menu',
+    initialState: {
+        isSaving: false,
+        messageSaved: '',
+        menus: [],
+        active: null,
+        // active: {
+        //     id:'ABC123',
+        //     title:'',
+        //     body:'',
+        //     date: 1232434,
+        //     imageURLs:[], // https://foto1.jpg https://foto2.jpg 
+        // }
+    },
+    reducers: {
+        savingNewNote: (state)=>{
+            state.isSaving=true;
+        },
+        addNewEmptyNote: (state,  action ) => {
+            state.notes.push(action.payload);
+            state.isSaving = false;
+        },
+        setActiveNote: (state,  action ) => {
+            state.active = action.payload;
+            state.messageSaved = ''
+        },
+        setNotes: (state,  action ) => {
+            state.notes = action.payload;
+        },
+        setSaving: (state ) => {
+            state.isSaving = true;
+            state.messageSaved = '' 
+        },
+        updateNote: (state,  action ) => {
+            state.isSaving = false;
+            state.notes = state.notes.map( note => {
+            
+                if ( note.id === action.payload.id ) {
+                    return action.payload;
+                }
+
+                return note;
+            });
+
+            state.messageSaved = `${ action.payload.title }, actualizada correectamente`
+
+        },
+
+        deleteNoteById: (state,  action ) => {
+            
+        },
+    }
+});
+
+// Action creators are generated for each case reducer function
+export const { addNewEmptyNote,setActiveNote ,setNotes, setSaving, updateNote, deleteNoteById, savingNewNote} = journalSlice.actions;
