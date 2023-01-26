7e9ed984763f92d3b9b7bf40b4507a914fa2c23d
NelsonKyros <nelson.soto@kyros.cl>
2023-01-26 16:52:03 -0300

    Actualiza Pauta

---
+++ src/__mocks__/pauta.js
@@ -0,0 +1,38 @@
+
+
+
+export const pauta = [
+    {
+      nombre: 'VOZ',
+      createdAt: '22/09/2022',
+      createdUp: '24/09/2022',
+      user: 'Juanito',
+      concepto: [
+        {
+            'id': '1',
+            'nombre': 'Conecta',
+            'total': '25',
+            categoria: [
+
+            ]
+        },
+        {
+            'id': '2',
+            'nombre': 'Comprende',
+            'total': '46'
+        },
+        {
+            'id': '3',
+            'nombre': 'Compromete',
+            'total': '20'
+        },
+        {
+            'id': '4',
+            'nombre': 'Cierre',
+            'total': '9'
+        },
+      ],
+        
+        
+    }
+]
