bdf4dc188897eeb845c0e3d4d4bb649fbcb2816b
NelsonKyros <nelson.soto@kyros.cl>
2023-03-21 11:23:46 -0300

    ok

--- src/journal/components/asignacion/GlobalFilter.jsx
+++ src/journal/components/asignacion/GlobalFilter.jsx
@@ -1,19 +1,3 @@
-// import React from 'react'
-
-//  const GlobalFilter = ({filter, setFilter}) => {
-//   return (
-//     <>
-      
-//         <span>
-
-//             Search:{' '}
-//             <input  value={filter || ''} onChange={e=> setFilter(e.target.value)}/>
-//         </span>
-//     </>
-//   )
-// }
-
-// export default GlobalFilter
 
 import { Box } from '@mui/material'
 import React from 'react'
