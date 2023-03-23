57d100c8b51edf5c9e7837953b16d052bf7e9aec
NelsonKyros <nelson.soto@kyros.cl>
2023-03-23 10:53:27 -0300

    Actualiza 23/03 1053

--- src/journal/components/asignacion/visual_formato/VisualFormato.jsx
+++ src/journal/components/asignacion/visual_formato/VisualFormato.jsx
@@ -15,7 +15,7 @@ const VisualFormato = ({id = ''}) => {
     let titulo=[];
     for (let index = 0; index < formatos[j].cabezaJson.length; index++) {
 
-        if (formatos[j].cabezaJson.length === 'Monitor') {
+        if (formatos[j].cabezaJson[index] === 'Monitor') {
             titulo.push({ title: formatos[j].cabezaJson[index],field: formatos[j].cabezaJson[index],align: "center", headerStyle: { color: "#2196f3" }
             ,lookup: { MONITOR1: "MONITOR 1", MONITOR2: "MONITOR 2", MONITOR3: "MONITOR 3", MONITOR4: "MONITOR 4", MONITOR5: "MONITOR 5", MONITOR6: "MONITOR 6", MONITOR7: "MONITOR 7" }});
         }
