b1d941ebea5f21df925cc9acb590c3d7e3046d25
NelsonKyros <nelson.soto@kyros.cl>
2023-04-13 18:52:27 -0400

    Auditoria

--- src/journal/components/auditoria/Auditoria_Preguntas.jsx
+++ src/journal/components/auditoria/Auditoria_Preguntas.jsx
@@ -56,7 +56,7 @@ const Auditoria_Preguntas = ({pautasSeleccion}) => {
   //convertir una cadena de texto pauta en formato JSON a un objeto de JavaScript
   const arreglo = JSON.parse(pauta);
 
-  //Recorre pauta para extración de preguntas
+  //Recorre pauta para extración de preguntas dejando estas agrupadas en bloque de evaluación
   const preguntasPorBloque = arreglo[0].detalleJson.reduce((acc, pregunta, i) => {
     const { 
       "BLOQUES DE EVALUACIÓN": bloque,
@@ -75,55 +75,56 @@ const Auditoria_Preguntas = ({pautasSeleccion}) => {
   
   return (
     <Card variant="outlined" sx={{ borderRadius: "12px", backgroundColor: "#f5f5f5", borderColor: "primary.main" }}>
-    {Object.entries(preguntasPorBloque).map(([bloque, preguntas]) => (
-      <CardContent key={bloque} sx={{ borderColor: "primary.main" }}>
-        <FormControl>
-          <FormLabel id={`demo-radio-buttons-group-label-${bloque}`} sx={{ textAlign: "center", backgroundColor: "#2196f3", borderRadius: "20px" ,fontWeight: "bold",
-    color: "white" }}>
-            {bloque}
-          </FormLabel>
-          <br />
-          {preguntas.map((pregunta) => (
-            <div key={pregunta.id}>
-              <p>{pregunta.text}</p>
-              <p>"{pregunta.prompt}"</p>
-              <RadioGroup
-                aria-labelledby={`demo-radio-buttons-group-label-${pregunta.id}`}
-                name={`radio-buttons-group-${pregunta.id}`}
-                onChange={(e) => handleRespuesta(pregunta.id, e.target.value)}
-              >
-                <FormControlLabel value="SI" control={<Radio />} label="SI" />
-                <FormControlLabel value="NO" control={<Radio />} label="NO" />
-              </RadioGroup>
-              {respuestas[pregunta.id]?.respuesta === "NO" && (
-                <TextField
-                  required
-                  label="Comentario"
-                  variant="outlined"
-                  value={respuestas[pregunta.id]?.comentario}
-                  onChange={(e) =>
-                    setRespuestas({
-                      ...respuestas,
-                      [pregunta.id]: {
-                        ...respuestas[pregunta.id],
-                        comentario: e.target.value,
-                      },
-                    })
-                  }
-                  margin="dense"
-                />
-              )}
-              <br />
-            </div>
-          ))}
-        </FormControl>
+  {Object.entries(preguntasPorBloque).map(([bloque, preguntas]) => (
+    <CardContent key={bloque} sx={{ borderColor: "primary.main" }}>
+      <FormControl>
+        <FormLabel id={`demo-radio-buttons-group-label-${bloque}`} sx={{ textAlign: "center", backgroundColor: "#2196f3", borderRadius: "20px" ,fontWeight: "bold",color: "white" }}>
+          {bloque}
+        </FormLabel>
         <br />
-        <Button sx={{ mx: "auto" }} onClick={handleSubmit}>
-          Guardar
-        </Button>
-      </CardContent>
-    ))}
-  </Card>
+        {preguntas.map((pregunta) => (
+          <div key={pregunta.id}>
+            <p>{pregunta.text}</p>
+            <p>"{pregunta.prompt}"</p>
+            <RadioGroup
+              aria-labelledby={`demo-radio-buttons-group-label-${pregunta.id}`}
+              name={`radio-buttons-group-${pregunta.id}`}
+              onChange={(e) => handleRespuesta(pregunta.id, e.target.value)}
+            >
+              <FormControlLabel value="SI" control={<Radio />} label="SI" />
+              <FormControlLabel value="NO" control={<Radio />} label="NO" />
+            </RadioGroup>
+            {respuestas[pregunta.id]?.respuesta === "NO" && (
+              <TextField
+                required
+                label="Comentario"
+                variant="outlined"
+                value={respuestas[pregunta.id]?.comentario}
+                onChange={(e) =>
+                  setRespuestas({
+                    ...respuestas,
+                    [pregunta.id]: {
+                      ...respuestas[pregunta.id],
+                      comentario: e.target.value,
+                    },
+                  })
+                }
+                margin="dense"
+              />
+            )}
+            <br />
+          </div>
+        ))}
+      </FormControl>
+      <br />
+    </CardContent>
+  ))}
+  <CardContent sx={{ borderColor: "primary.main" }}>
+    <Button sx={{ mx: "auto" }} onClick={handleSubmit}>
+      Guardar
+    </Button>
+  </CardContent>
+</Card>
   );
 };
 
