273a285b668796536aae09338abe3e308f5bbf0f
NelsonKyros <nelson.soto@kyros.cl>
2023-05-30 17:59:52 -0400

    ok

---
+++ src/journal/components/dashboard/RegistrosTotal.jsx
@@ -0,0 +1,82 @@
+import { Avatar, Box, Card, CardContent, Grid, Typography } from '@mui/material';
+import ArrowUpwardRoundedIcon from '@mui/icons-material/ArrowUpwardRounded';
+import AssignmentRoundedIcon from '@mui/icons-material/AssignmentRounded';
+import { useSelector } from 'react-redux';
+
+import React from 'react'
+
+const RegistrosTotal = () => {
+
+  const { formatos } = useSelector(state => state.formato);
+  const cantidadRegistros = formatos.length;
+
+  return (
+    <>
+     <Card
+    sx={{ height: '100%' }}
+    >
+    <CardContent>
+      <Grid
+        container
+        spacing={3}
+        sx={{ justifyContent: 'space-between' }}
+      >
+        <Grid item>
+          <Typography
+            color="textSecondary"
+            gutterBottom
+            variant="overline"
+          >
+            DOCUMENTOS ACTUALES
+          </Typography>
+          <Typography
+            color="textPrimary"
+            variant="h4"
+          >
+            {cantidadRegistros}
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Avatar
+            sx={{
+              backgroundColor: 'skyblue',
+              height: 56,
+              width: 56
+            }}
+          >
+            <AssignmentRoundedIcon />
+          </Avatar>
+        </Grid>
+      </Grid>
+      <Box
+        sx={{
+          pt: 2,
+          display: 'flex',
+          alignItems: 'center'
+        }}
+      >
+        <ArrowUpwardRoundedIcon color="success" />
+        <Typography
+          color="error"
+          sx={{
+            mr: 1
+          }}
+          variant="body2"
+        >
+          100%
+        </Typography>
+        <Typography
+          color="textSecondary"
+          variant="caption"
+        >
+          Desde mayo 2023
+        </Typography>
+      </Box>
+    </CardContent>
+  </Card>
+      
+    </>
+  )
+}
+
+export default RegistrosTotal
--- src/journal/pages/dashboard/DashboardPage.jsx
+++ src/journal/pages/dashboard/DashboardPage.jsx
@@ -9,6 +9,7 @@ import { Container } from '@mui/material';
 import { TotalCustomers } from '../../components/dashboard/total-customers';
 import { TasksProgress } from '../../components/dashboard/tasks-progress';
 import { TotalProfit } from '../../components/dashboard/total-profit';
+import RegistrosTotal from '../../components/dashboard/RegistrosTotal';
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -32,6 +33,15 @@ export const DashboardPage = () => {
             container
             spacing={3}
             >
+              <Grid
+              item
+              lg={3}
+              sm={6}
+              xl={3}
+              xs={12}
+              >
+                <RegistrosTotal />
+              </Grid>
               <Grid
               item
               lg={3}
